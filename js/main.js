1efcd597e1563869eefb3f0aaef16b00ce0cd145
Jake McKenzie <51035453+jake-mckenzie@users.noreply.github.com>
2019-10-28 15:58:25 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,81 +1,56 @@
 $(document).ready(function () {
+  $('.group, .group2, .group3, .modal').hide();
   $('.group').hide();
   $('#option1').show();
   $('#selectMe').change(function () {
     $('.group').hide();
     $('#'+$(this).val()).show();
   })
+  $('#selectMe2').change(function () {
+    $('.group2').hide();
+    $('#'+$(this).val()).show();
+  })
+  $('#selectMe3').change(function () {
+    $('.group3').hide();
+    $('#'+$(this).val()).show();
+  })
+  //      ===== Scroll to Top ====
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {
+      // If page is scrolled more than 50px
+      $('#return-to-top').fadeIn(200); // Fade in the arrow
+    } else {
+      $('#return-to-top').fadeOut(200); // Else fade out the arrow
+    }
+  });
+
+  $('#return-to-top').click(function() {
+    // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0, // Scroll to top of body
+      },
+      800
+    );
+  });
+
+  $("#requestForm").validate({
+      rules: {
+          authName: "required",
+          reqName: "required",
+          username: "required",
+          position: "required",
+      },
+      messages: {
+          authName: "Please enter name of Authorizer",
+          reqName: "Please enter name of Requester",
+          username: "Please enter name of User",
+          position: "Please enter position of User",
+      },
+      submitHandler: function (form) {
+          $("#myModal").modal('show');
+  		$('#SubForm').click(function () {
+              form.submit();
+         });
+      }
+  });
 });
-//
-// var x, i, j, selElmnt, a, b, c;
-// /*look for any elements with the class "custom-select":*/
-// x = document.getElementsByClassName("select");
-// for (i = 0; i < x.length; i++) {
-//   selElmnt = x[i].getElementsByTagName("select")[0];
-//   /*for each element, create a new DIV that will act as the selected item:*/
-//   a = document.createElement("DIV");
-//   a.setAttribute("class", "select-selected");
-//   a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-//   x[i].appendChild(a);
-//   /*for each element, create a new DIV that will contain the option list:*/
-//   b = document.createElement("DIV");
-//   b.setAttribute("class", "select-items select-hide");
-//   for (j = 1; j < selElmnt.length; j++) {
-//     /*for each option in the original select element,
-//     create a new DIV that will act as an option item:*/
-//     c = document.createElement("DIV");
-//     c.innerHTML = selElmnt.options[j].innerHTML;
-//     c.addEventListener("click", function(e) {
-//         /*when an item is clicked, update the original select box,
-//         and the selected item:*/
-//         var y, i, k, s, h;
-//         s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-//         h = this.parentNode.previousSibling;
-//         for (i = 0; i < s.length; i++) {
-//           if (s.options[i].innerHTML == this.innerHTML) {
-//             s.selectedIndex = i;
-//             h.innerHTML = this.innerHTML;
-//             y = this.parentNode.getElementsByClassName("same-as-selected");
-//             for (k = 0; k < y.length; k++) {
-//               y[k].removeAttribute("class");
-//             }
-//             this.setAttribute("class", "same-as-selected");
-//             break;
-//           }
-//         }
-//         h.click();
-//     });
-//     b.appendChild(c);
-//   }
-//   x[i].appendChild(b);
-//   a.addEventListener("click", function(e) {
-//       /*when the select box is clicked, close any other select boxes,
-//       and open/close the current select box:*/
-//       e.stopPropagation();
-//       closeAllSelect(this);
-//       this.nextSibling.classList.toggle("select-hide");
-//       this.classList.toggle("select-arrow-active");
-//     });
-// }
-// function closeAllSelect(elmnt) {
-//   /*a function that will close all select boxes in the document,
-//   except the current select box:*/
-//   var x, y, i, arrNo = [];
-//   x = document.getElementsByClassName("select-items");
-//   y = document.getElementsByClassName("select-selected");
-//   for (i = 0; i < y.length; i++) {
-//     if (elmnt == y[i]) {
-//       arrNo.push(i)
-//     } else {
-//       y[i].classList.remove("select-arrow-active");
-//     }
-//   }
-//   for (i = 0; i < x.length; i++) {
-//     if (arrNo.indexOf(i)) {
-//       x[i].classList.add("select-hide");
-//     }
-//   }
-// }
-// /*if the user clicks anywhere outside the select box,
-// then close all select boxes:*/
-// document.addEventListener("click", closeAllSelect);
