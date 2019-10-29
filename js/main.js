1a1f2016b29474d866ae52f69075719388ae3a0b
Jake McKenzie <51035453+jake-mckenzie@users.noreply.github.com>
2019-10-29 09:11:26 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('.group, .group2').hide();
+  $('.group, .group2, .group3, .modal').hide();
   $('.group').hide();
   $('#option1').show();
   $('#selectMe').change(function () {
@@ -10,4 +10,47 @@ $(document).ready(function () {
     $('.group2').hide();
     $('#'+$(this).val()).show();
   })
+  $('#selectMe3').change(function () {
+    $('.group3').hide();
+    $('#'+$(this).val()).show();
+  })
+  //      ===== Scroll to Top ====
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {
+      // If page is scrolled more than 50px
+      $('#return-to-top').fadeIn(200); // Fade in the arrow
+    } else {
+      $('#return-to-top').fadeOut(200); // Else fade out the arrow
+    }
+  });
+
+  $('#return-to-top').click(function() {
+    // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0, // Scroll to top of body
+      },
+      800
+    );
+  });
+
+  $("#requestForm").validate({
+      rules: {
+          authName: "required",
+          reqName: "required",
+          username: "required",
+          position: "required",
+      },
+      messages: {
+          authName: "Please enter name of Authorizer",
+          reqName: "Please enter name of Requester",
+          username: "Please enter name of User",
+          position: "Please enter position of User",
+      },
+      submitHandler: function (form) {
+          $("#myModal").modal('show');
+  		$('#SubForm').click(function () {
+              form.submit();
+         });
+      }
+  });
 });
