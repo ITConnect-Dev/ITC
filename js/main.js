f08ad247fd83e6c94269e78123a6a659242d9d9b
ITConnect-Dev <58284806+ITConnect-Dev@users.noreply.github.com>
2019-12-04 13:22:22 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,24 +5,31 @@
     return local.toJSON().slice(0,10);
   });
 
-$(document).ready(function () {
-
+$(document).ready(function ()
+{
   $('#datePicker').val(new Date().toDateInputValue());
   $('.group, .group2, .group3, .modal').hide();
-  $('.group').hide();
   $('#option1').show();
+  $('#bby, #bby2, #bbyc2, #bby3, #bby4').hide();
+  $('#edm, #edm2, #edmc2, #edm3, #edm4').hide();
+  $('#site, #site2, #sitec2, #site3, #site4').hide();
+
   $('#selectMe').change(function () {
     $('.group').hide();
     $('#'+$(this).val()).show();
-  })
+  });
+
   $('#selectMe2').change(function () {
     $('.group2').hide();
     $('#'+$(this).val()).show();
-  })
+  });
+
   $('#selectMe3').change(function () {
     $('.group3').hide();
     $('#'+$(this).val()).show();
-  })
+  });
+
+
   //      ===== Scroll to Top ====
   $(window).scroll(function() {
     if ($(this).scrollTop() >= 50) {
@@ -62,4 +69,145 @@ $(document).ready(function () {
           });
       }
   });
+
+  $( "#location" ).change( function() {
+    var s = this.value;
+    if(s == "bby")
+    {
+      $('#bby').show();
+      $('#edm').hide();
+      $('#site').hide();
+    }
+    else if(s == "edm")
+    {
+      $('#bby').hide();
+      $('#edm').show();
+      $('#site').hide();
+    }
+    else if(s == "site")
+    {
+      $('#bby').hide();
+      $('#edm').hide();
+      $('#site').show();
+    }
+    else
+    {
+      $('#bby').hide();
+      $('#edm').hide();
+      $('#site').hide();
+    }
+  });
+
+  $( "#location2" ).change( function() {
+    var s = this.value;
+    if(s == "bby2")
+    {
+      $('#bby2').show();
+      $('#edm2').hide();
+      $('#site2').hide();
+    }
+    else if(s == "edm2")
+    {
+      $('#bby2').hide();
+      $('#edm2').show();
+      $('#site2').hide();
+    }
+    else if(s == "site2")
+    {
+      $('#bby2').hide();
+      $('#edm2').hide();
+      $('#site2').show();
+    }
+    else
+    {
+      $('#bby2').hide();
+      $('#edm2').hide();
+      $('#site2').hide();
+    }
+  });
+
+  $( "#currentLocation2" ).change( function() {
+    var s = this.value;
+    if(s == "bbyc2")
+    {
+      $('#bbyc2').show();
+      $('#edmc2').hide();
+      $('#sitec2').hide();
+    }
+    else if(s == "edmc2")
+    {
+      $('#bbyc2').hide();
+      $('#edmc2').show();
+      $('#sitec2').hide();
+    }
+    else if(s == "sitec2")
+    {
+      $('#bbyc2').hide();
+      $('#edmc2').hide();
+      $('#sitec2').show();
+    }
+    else
+    {
+      $('#bbyc2').hide();
+      $('#edmc2').hide();
+      $('#sitec2').hide();
+    }
+  });
+
+  $( "#location3" ).change( function() {
+    var s = this.value;
+    if(s == "bby3")
+    {
+      $('#bby3').show();
+      $('#edm3').hide();
+      $('#site3').hide();
+    }
+    else if(s == "edm3")
+    {
+      $('#bby3').hide();
+      $('#edm3').show();
+      $('#site3').hide();
+    }
+    else if(s == "site3")
+    {
+      $('#bby3').hide();
+      $('#edm3').hide();
+      $('#site3').show();
+    }
+    else
+    {
+      $('#bby3').hide();
+      $('#edm3').hide();
+      $('#site3').hide();
+    }
+  });
+
+  $( "#location4" ).change( function() {
+    var s = this.value;
+    if(s == "bby4")
+    {
+      $('#bby4').show();
+      $('#edm4').hide();
+      $('#site4').hide();
+    }
+    else if(s == "edm4")
+    {
+      $('#bby4').hide();
+      $('#edm4').show();
+      $('#site4').hide();
+    }
+    else if(s == "site4")
+    {
+      $('#bby4').hide();
+      $('#edm4').hide();
+      $('#site4').show();
+    }
+    else
+    {
+      $('#bby4').hide();
+      $('#edm4').hide();
+      $('#site4').hide();
+    }
+  });
+
 });
